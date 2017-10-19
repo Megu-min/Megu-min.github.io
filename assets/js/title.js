645bc8d6a07c741a5e92f6c02c7a4a8b14519855
Megu-min <32789791+Megu-min@users.noreply.github.com>
2017-10-19 10:08:57 -0400

    Update title.js

--- assets/js/title.js
+++ assets/js/title.js
@@ -16,7 +16,7 @@ function loaded(){
 
 var x = 0;
 
-var titleText = ["$", "$ m", "$ me", "$ meg", "$ megu", "$ megum", "$ megumi", "$ megumin", "$ megumin.", "$ megumin.t", "$ megumin.te", "$ megumin.tea", "$ megumin.team", "$ megumin.tea",  "$ megumin.te",  "$ megumin.t", "$ megumin.", "$ megumin", "$ megumi", "$ megum",  "$ megu",  "$ meg",  "$ me", " $ m"]; //pHard coded xd Nyctum
+var titleText = ["$", "$ m", "$ me", "$ meg", "$ megu", "$ megum", "$ megumi", "$ megumin", "$ megumin.", "$ megumin.t", "$ megumin.te", "$ megumin.tea", "$ megumin.team", "$ megumin.tea",  "$ megumin.te",  "$ megumin.t", "$ megumin.", "$ megumin", "$ megumi", "$ megum",  "$ megu",  "$ meg",  "$ me", " $ m"];
 
 function loop(){
 
